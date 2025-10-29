afc63d10490e09aa0b4a11f666fa823df3104cbd
mantana-chp <mantana.kodc@mail.kmutt.ac.th>
2025-10-29 13:20:39 +0700

    [FE]🌷add: style for OS screen

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
  
 import React from 'react'
 import CalendarPage from '@/presentation/calendar/pages/calendar_page'
+import { ThemedView } from '@/components/themed-view'
+import { Platform, StatusBar } from 'react-native'
 
 export default function CalendarTab() {
   return (
-    <CalendarPage />
+    <ThemedView
+          style={{
+            flex: 1,
+            padding: 10,
+            paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 10
+          }}
+        >
+    <CalendarPage /></ThemedView>
   )
 }
