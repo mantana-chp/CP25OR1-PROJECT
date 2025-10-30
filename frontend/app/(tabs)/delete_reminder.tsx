a032f27e7d846d0953e5f69be74b6fcee5f7c3bb
paepilai <ioonooni@gmail.com>
2025-10-30 15:32:34 +0700

    [FE] 🏰 remove: trying to do delete reminder

--- frontend/app/(tabs)/delete_reminder.tsx
+++
@@ -1,9 +0,0 @@
- 
-import React from 'react'
-import DeleteReminderPage from '@/presentation/reminder/pages/delete_reminder_page'
-
-export default function DeleteReminderTab() {
-  return (
-    <DeleteReminderPage />
-  )
-}
