7fc636b449e04e4de4b668759af5986e542ff1b5
mantana-chp <mantana.kodc@mail.kmutt.ac.th>
2025-10-30 17:43:16 +0700

    [FE]🌷remove: duplicate tabs route

--- frontend/app/(tabs)/calendar.tsx
+++
@@ -1,9 +0,0 @@
- 
-import React from 'react'
-import CalendarPage from '@/src/presentation/calendar/pages/calendar_page'
-
-export default function CalendarTab() {
-  return (
-    <CalendarPage />
-  )
-}
--- frontend/app/(tabs)/pet_profile.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-import PetProfilePage from '@/src/presentation/pet_profile/pages/pet_profile_page'
-
-export default function PetProfileScreen() {
-  return (
-    <PetProfilePage />
-  )
-}
