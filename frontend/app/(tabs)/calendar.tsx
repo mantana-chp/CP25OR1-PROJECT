d93727e65191f706f0f7bec3430837e60ccaa3e4
mantana-chp <mantana.kodc@mail.kmutt.ac.th>
2025-10-30 17:40:59 +0700

    [FE]🌷fix: more conflict

--- frontend/app/(tabs)/calendar.tsx
+++ frontend/app/(tabs)/calendar.tsx
@@ -1,6 +1,6 @@
  
 import React from 'react'
-import CalendarPage from '@/presentation/calendar/pages/calendar_page'
+import CalendarPage from '@/src/presentation/calendar/pages/calendar_page'
 
 export default function CalendarTab() {
   return (
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,31 +1,8 @@
-<<<<<<< HEAD
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider
-} from '@react-navigation/native'
-=======
 import { DefaultTheme, ThemeProvider } from '@react-navigation/native'
->>>>>>> 38c038a3db8ec64c853c8e07c9de93637a272fdc
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import 'react-native-reanimated'
 
-<<<<<<< HEAD
-import { NotificationProvider } from '@/context/NotificationContext'
-import { useColorScheme } from '@/hooks/use-color-scheme'
-
-import * as Notifications from 'expo-notifications'
-import '@/global.css'
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
-import * as SplashScreen from 'expo-splash-screen'
-
-import {
-  useFonts,
-  Prompt_400Regular,
-  Prompt_500Medium,
-  Prompt_700Bold
-=======
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider'
 import { NotificationProvider } from '@/context/NotificationContext'
 import { useColorScheme } from '@/src/hooks/use-color-scheme'
@@ -39,7 +16,6 @@ import {
   Prompt_500Medium,
   Prompt_700Bold,
   useFonts
->>>>>>> 38c038a3db8ec64c853c8e07c9de93637a272fdc
 } from '@expo-google-fonts/prompt'
 
 import { useCallback } from 'react'
@@ -75,17 +51,9 @@ export default function RootLayout() {
   if (!fontsLoaded) return null
 
   return (
-<<<<<<< HEAD
-    <GluestackUIProvider mode="dark">
-      <NotificationProvider>
-        <ThemeProvider
-          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
-=======
     <GluestackUIProvider mode="light">
       <NotificationProvider>
         <ThemeProvider value={DefaultTheme}>
->>>>>>> 38c038a3db8ec64c853c8e07c9de93637a272fdc
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen
