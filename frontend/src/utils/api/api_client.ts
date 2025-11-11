69613184218d770337951ee46732f25911c549b7
mantana-chp <mantana.kodc@mail.kmutt.ac.th>
2025-11-11 23:59:15 +0700

    [FE]🌷clean: console.log

--- frontend/src/utils/api/api_client.ts
+++ frontend/src/utils/api/api_client.ts
@@ -93,7 +93,6 @@ class ApiClient {
   }
 
   private setupInterceptors() {
-    // Request interceptor - Add auth token
     this.client.interceptors.request.use(
       async (config) => {
         try {
