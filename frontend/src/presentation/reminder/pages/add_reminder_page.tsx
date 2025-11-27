b32c562b7025075038546c31c4042d9cd8047cea
mantana-chp <mantana.kodc@mail.kmutt.ac.th>
2025-11-27 20:38:25 +0700

    [FE]🌷update: default category for create reminder

--- frontend/src/presentation/reminder/pages/add_reminder_page.tsx
+++ frontend/src/presentation/reminder/pages/add_reminder_page.tsx
@@ -42,7 +42,12 @@ export default function AddReminderPage() {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
-      await createReminderApi.execute(values as IReminder)
+      const submitData = {
+        ...values,
+        categoryName: values.categoryName || 'General'
+      }
+
+      await createReminderApi.execute(submitData)
       formik.resetForm()
     }
   })
