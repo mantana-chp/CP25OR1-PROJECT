88aff7c4e3982b224146a97909a271fc06f7ebe2
mantana-chp <mantana.kodc@mail.kmutt.ac.th>
2026-03-22 17:10:02 +0700

    [BE]🌷add: loggedAt field

--- backend/src/features/health-log/health-log-types.ts
+++ backend/src/features/health-log/health-log-types.ts
@@ -1,18 +1,19 @@
 export interface HealthLogDto {
-  id: string;
-  petId: string;
-  createdByUserId: string;
-  createdBy: string;
-  description: string;
-  weight?: number;
-  note?: string;
-  loggedAt: Date;
-  createdAt: Date;
-  updatedAt: Date;
+  id: string
+  petId: string
+  createdByUserId: string
+  createdBy: string
+  description: string
+  weight?: number
+  note?: string
+  loggedAt: Date
+  createdAt: Date
+  updatedAt: Date
 }
 
 export interface CreateHealthLogInput {
-  description: string;
-  weight?: number;
-  note?: string;
+  description: string
+  weight?: number
+  note?: string
+  loggedAt?: Date
 }
